3c079c532192a31266fdf6ac879a5165ead30703
Onur Erinç ŞENOL <82088135+ErincSENOL@users.noreply.github.com>
2023-03-14 22:28:41 +0300

    Add files via upload

---
+++ SqLiteNoDb/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SqLiteNoDb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblPassTime = new System.Windows.Forms.Label();
+            this.txtComment = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgWorkList = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StopTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgWorkList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(8, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(574, 264);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage1.Controls.Add(this.btnStop);
+            this.tabPage1.Controls.Add(this.lblPassTime);
+            this.tabPage1.Controls.Add(this.txtComment);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(566, 238);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Yeni Kayıt";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(404, 210);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Kaydet";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblPassTime
+            // 
+            this.lblPassTime.AutoSize = true;
+            this.lblPassTime.Location = new System.Drawing.Point(6, 7);
+            this.lblPassTime.Name = "lblPassTime";
+            this.lblPassTime.Size = new System.Drawing.Size(0, 13);
+            this.lblPassTime.TabIndex = 3;
+            // 
+            // txtComment
+            // 
+            this.txtComment.Location = new System.Drawing.Point(6, 23);
+            this.txtComment.Multiline = true;
+            this.txtComment.Name = "txtComment";
+            this.txtComment.Size = new System.Drawing.Size(554, 180);
+            this.txtComment.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(485, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnDelete);
+            this.tabPage2.Controls.Add(this.dgWorkList);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(566, 238);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Kayıt Listesi";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(441, 212);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgWorkList
+            // 
+            this.dgWorkList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgWorkList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.StartTime,
+            this.StopTime,
+            this.Comment});
+            this.dgWorkList.Location = new System.Drawing.Point(7, 7);
+            this.dgWorkList.Name = "dgWorkList";
+            this.dgWorkList.Size = new System.Drawing.Size(543, 200);
+            this.dgWorkList.TabIndex = 0;
+            this.dgWorkList.SelectionChanged += new System.EventHandler(this.dgWorkList_SelectionChanged);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 30;
+            // 
+            // StartTime
+            // 
+            this.StartTime.DataPropertyName = "StartTime";
+            this.StartTime.HeaderText = "StartTime";
+            this.StartTime.Name = "StartTime";
+            this.StartTime.ReadOnly = true;
+            // 
+            // StopTime
+            // 
+            this.StopTime.DataPropertyName = "StopTime";
+            this.StopTime.HeaderText = "StopTime";
+            this.StopTime.Name = "StopTime";
+            this.StopTime.ReadOnly = true;
+            // 
+            // Comment
+            // 
+            this.Comment.DataPropertyName = "Comment";
+            this.Comment.HeaderText = "Comment";
+            this.Comment.Name = "Comment";
+            this.Comment.ReadOnly = true;
+            this.Comment.Width = 270;
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 282);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgWorkList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtComment;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgWorkList;
+        private System.Windows.Forms.Label lblPassTime;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmrTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StartTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StopTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
+    }
+}
+
---
+++ SqLiteNoDb/Form1.cs
@@ -0,0 +1,105 @@
+﻿using LiteDB;
+using SqLiteNoDb.Entities;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SqLiteNoDb
+{
+    public partial class Form1 : Form
+    {
+        private LiteDatabase _liteDatabase;
+        private LiteCollection<Work> _workCollection;
+        private int _createdItemId;
+        private int _selectedWorkId;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _liteDatabase = new LiteDatabase(@"D:/CalismaSurelerim.db");
+            _workCollection = GetAllWork(_liteDatabase);
+            WorkListDataGrid(_workCollection);
+            tmrTime.Interval= 1000;
+        }
+        public void WorkListDataGrid(LiteCollection<Work> _workListCollection)
+        {
+            var workAllList = _workListCollection.FindAll().ToList();
+            dgWorkList.DataSource = workAllList;
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Work work = new Work();
+            work.StopTime = DateTime.Now;
+            work.Comment = txtComment.Text;
+            Work createdWorkData = GetWorkById(_createdItemId);
+            if (createdWorkData != null)
+            {
+                work.StartTime = createdWorkData.StartTime;
+            }
+           
+            UpdateWork(work);
+            WorkListDataGrid(_workCollection);
+            tmrTime.Stop();
+            lblPassTime.Text = "";
+            _createdItemId = 0;
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Work work = new Work
+            {
+                StartTime = DateTime.Now
+            };
+            _createdItemId = AddWork(work);
+            WorkListDataGrid(_workCollection);
+            tmrTime.Start();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteWork(_selectedWorkId);
+            WorkListDataGrid(_workCollection);
+        }
+        private void dgWorkList_SelectionChanged(object sender, EventArgs e)
+        {
+            _selectedWorkId = Convert.ToInt32(dgWorkList.CurrentRow.Cells[0].Value);
+        }
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            if (_createdItemId > 0)
+            {
+                Work selectedWork = GetWorkById(_createdItemId);
+                if (selectedWork != null)
+                {
+                    TimeSpan timeDifference = DateTime.Now - selectedWork.StartTime;
+                    lblPassTime.Text = timeDifference.ToString("hh':'mm':'ss");
+                }
+            }
+        }
+        public LiteCollection<Work> GetAllWork(LiteDatabase database)
+        {
+            return _workCollection = (LiteCollection<Work>)database.GetCollection<Work>("Work");
+        }
+
+        public Work GetWorkById(int id)
+        {
+            var work = _workCollection.FindById(id);
+            return work;
+        }
+        public void UpdateWork(Work work)
+        {
+            _workCollection.Update(_createdItemId, work);
+        }
+        public int AddWork(Work work)
+        {
+            var result = _workCollection.Insert(work);
+            return (int)result.RawValue;
+        }
+        public void DeleteWork(int id)
+        {
+            var work = _workCollection.Delete(id);
+        }
+
+    }
+}
